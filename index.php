6ce9171aab0ed5aa23dd54791bc76e6d113b0839
Adrian Kuśmierek <huskyusadoggy@gmail.com>
2022-12-01 23:38:56 +0100

    x

--- index.php
+++ index.php
@@ -6,9 +6,9 @@ if (!isset($_GET["m"])) $_GET["m"] = 1;
 $smarty = new Smarty;
 $html = "";
 $pointer = $_GET["m"];
-$month_length = cal_days_in_month(CAL_GREGORIAN, $_GET["m"], 2022);
-$month_length_next = ($_GET["m"] == 1) ? cal_days_in_month(CAL_GREGORIAN, 1, 2022) : cal_days_in_month(CAL_GREGORIAN, $_GET["m"] - 1, 2022);
-$month_length_prev = ($_GET["m"] == 12) ? cal_days_in_month(CAL_GREGORIAN, 1, 2022) : cal_days_in_month(CAL_GREGORIAN, $_GET["m"] + 1, 2022);
+$month_length = cal_days_in_month(CAL_GREGORIAN, $pointer, 2022);
+$month_length_next = ($pointer == 1) ? cal_days_in_month(CAL_GREGORIAN, 12, 2022) : cal_days_in_month(CAL_GREGORIAN, $pointer - 1, 2022);
+$month_length_prev = ($pointer == 12) ? cal_days_in_month(CAL_GREGORIAN, 1, 2022) : cal_days_in_month(CAL_GREGORIAN, $pointer + 1, 2022);
 $y = date_format(date_create(date("Y-$pointer-01")), "N");
 
 for ($i = $month_length_prev + 2 - $y; $i <= $month_length_prev; $i++) {
@@ -18,7 +18,7 @@ for ($i = $month_length_prev + 2 - $y; $i <= $month_length_prev; $i++) {
 for ($i = 1; $i <= $month_length; $i++) {
     $_x = date("N");
 
-    if ($i == date("d") && date("m") == $_GET["m"]) {
+    if ($i == date("d") && date("m") == $pointer) {
         $html .= "<div class='now days' style='grid-column: $_x'>" . $i . "</div>";
     } else if ($i == 1) {
         $html .= "<div class='days' style='grid-column: $y'>" . $i . "</div>";
@@ -35,7 +35,7 @@ for ($i = 1; $i <= $month_length_next - (18 + $y); $i++) {
     }
 }
 
-$smarty->assign("title", DateTime::createFromFormat('!m', $_GET["m"])->format("F"));
+$smarty->assign("title", DateTime::createFromFormat('!m', $pointer)->format("F"));
 $smarty->assign("html", $html);
 $smarty->display("index.tpl");
 ?>
